bcd24d17958260f85b76d210e1f3c883f6a49512
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-12-09 18:43:42 -0300

    feat: search function

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <div id="app"></div>
+  <h4 class="is-size-4">Pokedex</h4>
   <div class="column is-half is-offset-one-quarter">
-    <div v-for="(poke, index) in pokemons" :key="index">
+    <input
+      type="text"
+      placeholder="Buscar pokemon pelo nome"
+      v-model="busca"
+      class="input is-rounded"
+    />
+    <button
+      class="button is-fullwidth is-success"
+      id="buscarBtn"
+      @click="buscar"
+    >
+      Buscar
+    </button>
+    <div v-for="(poke, index) in filterPokemons" :key="poke.url">
       <Pokemon :name="poke.name" :num="index + 1" :url="poke.url" />
     </div>
   </div>
@@ -16,6 +30,8 @@ export default {
   data() {
     return {
       pokemons: [],
+      filterPokemons: [],
+      busca: "",
     };
   },
   created: function () {
@@ -23,7 +39,20 @@ export default {
       .get("https://pokeapi.co/api/v2/pokemon?limit=15&offset=0")
       .then((data) => {
         this.pokemons = data.data.results;
+        this.filterPokemons = data.data.results;
       });
+  },
+  methods: {
+    buscar: function () {
+      if (this.busca === "") {
+        this.filterPokemons = this.pokemons; 
+      } else {
+        const buscaMinuscula = this.busca.toLowerCase(); 
+        this.filterPokemons = this.pokemons.filter((pokemon) =>
+          pokemon.name.toLowerCase().includes(buscaMinuscula)
+        );
+      }
+    },
   },
   components: {
     Pokemon,
@@ -39,5 +68,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#buscarBtn {
+  margin-top: 2%;
 }
 </style>
